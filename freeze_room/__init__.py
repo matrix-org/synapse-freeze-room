5f86b1815ad670f35dc2f8166e2ad9d0cd62a85a
Brendan Abolivier <babolivier@matrix.org>
2021-07-28 18:54:57 +0100

    Actually freeze room when the last admin leaves

--- freeze_room/__init__.py
+++ freeze_room/__init__.py
@@ -1,3 +1,4 @@
+import logging
 from typing import List, Tuple, Optional
 
 from synapse.events import EventBase
@@ -6,6 +7,8 @@ from synapse.types import StateMap
 from synapse.util.frozenutils import unfreeze
 
 from freeze_room._constants import EventTypes, Membership
+
+logger = logging.getLogger(__name__)
 
 FROZEN_STATE_TYPE = "org.matrix.room.frozen"
 
@@ -39,14 +42,30 @@ class RoomFreeze:
             dictionary if the event needs to be rebuilt (containing the event's new
             content).
         """
-        if event.type == FROZEN_STATE_TYPE:
+        if event.type == FROZEN_STATE_TYPE and event.is_state():
             return await self._on_frozen_state_change(event, state_events)
 
         # If the room is frozen, we allow a very small number of events to go through
         # (unfreezing, leaving, etc.).
         frozen_state = state_events.get((FROZEN_STATE_TYPE, ""))
-        if frozen_state and frozen_state.content.get("frozen", False):
+        if frozen_state and frozen_state.content.get("frozen", False) is True:
             return await self._on_event_when_frozen(event, state_events)
+
+        # If the event is a leave membership update, check if the last admin is leaving
+        # the room and freeze it if not.
+        if (
+            event.type == EventTypes.Member
+            and event.membership == Membership.LEAVE
+            and event.is_state()
+        ):
+            has_frozen_room = await self._freeze_room_if_last_admin_is_leaving(
+                event, state_events,
+            )
+            replacement = None
+            if has_frozen_room:
+                replacement = event.get_dict()
+
+            return True, replacement
 
     async def _on_frozen_state_change(
         self,
@@ -167,6 +186,77 @@ class RoomFreeze:
                     return True, None
 
         return False, None
+
+    async def _freeze_room_if_last_admin_is_leaving(
+        self, event: EventBase, state_events: StateMap[EventBase]
+    ) -> bool:
+        """Checks if the given leave event is for the last admin in the room, and if so
+        freezes the room by sending an org.matrix.room.frozen event.
+
+        Args:
+            event: The leave event to process
+            state_events: The current state of the room
+
+        Returns:
+            Whether the room was frozen.
+        """
+        power_level_state_event = state_events.get(
+            (EventTypes.PowerLevels, "")
+        )  # type: EventBase
+        if not power_level_state_event:
+            return False
+        power_level_content = power_level_state_event.content
+
+        # Do some validation checks on the power level state event
+        if (
+            not isinstance(power_level_content, dict)
+            or "users" not in power_level_content
+            or not isinstance(power_level_content["users"], dict)
+        ):
+            # We can't use this power level event to determine whether the room should be
+            # frozen. Bail out.
+            return False
+
+        user_id = event.get("sender")
+        if not user_id:
+            return False
+
+        # Get every admin user defined in the room's state
+        admin_users = {
+            user
+            for user, power_level in power_level_content["users"].items()
+            if power_level >= 100
+        }
+
+        if user_id not in admin_users:
+            # This user is not an admin, ignore them
+            return False
+
+        if any(
+            event_type == EventTypes.Member
+            and event.membership in [Membership.JOIN, Membership.INVITE]
+            and state_key in admin_users
+            and state_key != user_id
+            for (event_type, state_key), event in state_events.items()
+        ):
+            # There's another admin user in, or invited to, the room
+            return False
+
+        # Freeze the room by raising the required power level to send events to 100
+        logger.info("Freezing room '%s'", event.room_id)
+
+        # Mark the room as frozen
+        await self._api.create_and_send_event_into_room(
+            {
+                "room_id": event.room_id,
+                "sender": user_id,
+                "type": FROZEN_STATE_TYPE,
+                "content": {"frozen": True},
+                "state_key": "",
+            }
+        )
+
+        return True
 
     def _is_local_user(self, user_id: str) -> bool:
         """Checks whether a given user ID belongs to this homeserver, or a remote
