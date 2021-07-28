5c01b9789e4a45144f0806cf2a6d4a633e3fdeba
Brendan Abolivier <babolivier@matrix.org>
2021-07-28 09:58:02 +0100

    Add tests to check PL updates

--- tests/test_room_freeze.py
+++ tests/test_room_freeze.py
@@ -27,7 +27,7 @@ class RoomFreezeTest(aiounittest.AsyncTestCase):
                             "m.room.name": 50,
                             "m.room.power_levels": 100,
                             "m.room.server_acl": 100,
-                            "m.room.tombstone": 100
+                            "m.room.tombstone": 100,
                         },
                         "events_default": 0,
                         "invite": 0,
@@ -35,7 +35,8 @@ class RoomFreezeTest(aiounittest.AsyncTestCase):
                         "redact": 50,
                         "state_default": 50,
                         "users": {
-                            self.user_id: 100
+                            self.user_id: 100,
+                            "@mod:example.com": 50,
                         },
                         "users_default": 0
                     },
@@ -76,6 +77,34 @@ class RoomFreezeTest(aiounittest.AsyncTestCase):
             self.state,
         )
         self.assertFalse(allowed)
+
+    async def test_power_levels_sent_when_freezing(self):
+        pl_event_dict = await self._send_frozen_event_and_get_power_levels_update(True)
+
+        self.assertEqual(pl_event_dict["content"]["users_default"], 100)
+        for user, pl in pl_event_dict["content"]["users"].items():
+            self.assertEqual(pl, 100, user)
+
+    async def test_power_levels_sent_when_unfreezing(self):
+        pl_event_dict = await self._send_frozen_event_and_get_power_levels_update(False)
+
+        self.assertEqual(pl_event_dict["content"]["users_default"], 0)
+        self.assertEqual(pl_event_dict["content"]["users"][self.user_id], 100)
+
+    async def _send_frozen_event_and_get_power_levels_update(self, frozen: bool) -> dict:
+        module = create_module()
+        allowed, _ = await module.check_event_allowed(
+            self._build_frozen_event(sender=self.user_id, frozen=frozen),
+            self.state,
+        )
+        self.assertTrue(allowed)
+
+        self.assertTrue(module._api.create_and_send_event_into_room.called)
+        args, _ = module._api.create_and_send_event_into_room.call_args
+        self.assertEqual(len(args), 1)
+        self.assertEqual(args[0]["type"], EventTypes.PowerLevels)
+
+        return args[0]
 
     def _build_frozen_event(self, sender: str, frozen: bool) -> FrozenEventV3:
         event_dict = {
